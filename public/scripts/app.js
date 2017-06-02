6bf621ecd564dcfb7ba71d1483050bd3d79286c3
younjiwoo <younjiwoo@gmail.com>
2017-06-01 18:20:11 -0700

    made an ajax call and appended data on homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
     console.log('sanity check!');
-    $('body').append('<h2>test working</h2>');
-})
+    
+    $.ajax({
+        method: 'GET',
+        url: '/api/messages',
+        success: displayMessages,
+        error: errorMessage
+    });
+});
+
+function displayMessages (data) {
+    data.forEach(function (oneMessageObj) {
+        $('#messageBoard').append(`
+        <p>Title: ${oneMessageObj.title}</p>
+        <p>Address: ${oneMessageObj.address}</p>
+        <p>Rating: ${oneMessageObj.rating}</p>
+        <p>Message: ${oneMessageObj.message}</p>
+        `);
+    })
+}
+
+function errorMessage (error) {
+    console.log(err);
+}
