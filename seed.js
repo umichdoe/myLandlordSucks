f9c33b6249ea42688eaead333c628a8aca4c8dc7
younjiwoo <younjiwoo@gmail.com>
2017-06-01 17:33:54 -0700

    added api documentation endpoint

---
+++ seed.js
@@ -0,0 +1,39 @@
+//1. https://www.rentalutions.com/education/articles/when-renting-goes-wrong-6-tenant-horror-stories/
+
+//2. https://consumerist.com/2015/09/08/san-francisco-landlord-charged-4kmonth-in-rent-for-rodent-infested-death-trap-apartments/
+
+//3. http://www.sfchronicle.com/business/article/most-bizarre-airbnb-feud-story-6824921.php
+
+//4. https://thebolditalic.com/craigslist-housing-horror-stories-the-bold-italic-san-francisco-3fb7df5d4203
+
+//5. http://www.sfgate.com/bayarea/article/Landlord-nightmare-in-eviction-attempt-3849250.php
+
+//6. https://thebillfold.com/san-francisco-airbnb-horror-story-recalls-pacific-heights-almost-perfectly-7b3810e4b2b0
+
+//7. http://brokeassstuart.com/blog/2016/08/17/draft-sf-housing-crisis-horror-story-part-2/
+
+//8. http://www.huffingtonpost.com/2014/07/29/airbnb-horror-stories_n_5614452.html
+
+//9 https://handsontap.com/california-landlord-horror-stories/
+var db = require("./models");
+
+var messageList = [];
+
+messageList.push({
+    title: "My Landlord's dog attacked me!",
+    address: "225 Bush St. 5th Floor, San Francisco, CA 94104",
+    rating: 0,
+    message: "I was statying at an AirBnB"
+});
+
+
+db.Message.remove({}, function(err, albums){
+
+  db.Message.create(messageList, function(err, messages){
+    if (err) { return console.log('ERROR', err); }
+    console.log("all messages:", messages);
+    console.log("created", messageList.length, "albums");
+    process.exit();
+  });
+
+});
--- server.js
+++ server.js
@@ -8,6 +8,29 @@ app.get('/', function homepage (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+// Index of Messages
+//app.get('/api/messages', function index (req, res) {
+//    db.Message.find({}, function(err, allMessages) {
+//        res.json(allMessages);
+//    });
+//});
+
+app.get('/api', function apiIndex(req, res, next) {
+    res.json({
+        project: "My Landlord Sucks App",
+        message: "Welcome to My Landlord Sucks App api. Here is what you need to know.",
+        documentationUrl: "https://github.com/younjiwoo/myLandlordSucks/blob/master/readme.md",
+        baseUrl: "https://boiling-earth-58583.herokuapp.com/",
+        endpoints: [
+            {method: "GET", path: "/api", description: "Describes all available endpoints."},
+            {method: "GET", path: "/api/messages", description: "Index of all the messages and complaints of unhappy tenants."}, // DONE #3
+            {method: "POST", path: "/api/messages", description: "Create or add a new message"}, // DONE #4
+            {method: "PUT", path: "/api/messages/:id", description: "Edit a message and update it."}, //  DONE #5
+            {method: "DELETE", path: "/api/messages/:id", description: "Destroy a message."} // #6
+        ]
+    })
+});
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express server is running on http://localhost:3000/');
 });
