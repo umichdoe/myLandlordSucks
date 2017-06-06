d0692142d977d80f85168e899d9841c525dd3e36
younjiwoo <younjiwoo@gmail.com>
2017-06-06 15:53:50 -0700

    changed address to city

--- seed.js
+++ seed.js
@@ -29,70 +29,70 @@ var messageList = [];
 
 messageList.push({
   title: "My landord painted a rainbow on my house",
-  address: "225 Bush St. 5th Floor, San Francisco, CA 94104",
+  address: "San Francisco, CA 94104",
   rating: 5,
   imgURL: "https://s-media-cache-ak0.pinimg.com/736x/d3/68/74/d36874eee05c8b7500747cc849cb9c34.jpg",
-  message: "I am getting weird visitors since this paint job. My landlord didn't even consult me."
+  message: "I love it! :)"
 });
 messageList.push({
   title: "The house is sinking into the ground like the leaning tower of Pisa",
-  address: "999 Main St., San Francisco, CA 94103",
+  address: "San Francisco, CA 94103",
   rating: 1,
   imgURL: "http://noehill.com/sf/landmarks/haight/delano_house_thumb.jpg",
   message: "I keep falling off my bed because I keep rolling to one side."
 });
 messageList.push({
   title: "The landlord's drunk friend showed up and slept in my room",
-  address: "103 Harrison St. 19th Fl, San Francisco, CA 94101",
+  address: "Berkeley, CA 94101",
   rating: 2,
   imgURL: "https://static01.nyt.com/images/2010/11/22/nyregion/22cityroom-ready/22cityroom-ready-blog480.jpg",
   message: "I will pay you to live here."
 });
 messageList.push({
   title: "The oven smokes like a barbeque pit.",
-  address: "1234 sunrise st. San Francisco, CA 94321",
+  address: "Redwood City, CA 94321",
   rating: 3,
   imgURL: "https://media.giphy.com/media/3o6ZsUW74p4cvyD2ow/giphy.gif",
   message: "If this oven doesn't get fixed soon, I'm afraid the house will burn down.",
 });
 messageList.push({
   title: "There's a man dancing on our roof.",
-  address: "85 Texas st. San Diego, CA 92111",
+  address: "San Diego, CA 92111",
   rating: 5,
   imgURL: "https://media.giphy.com/media/TjbN05AFbgOnm/giphy.gif",
   message: "I wanna have a conversation with him but he wouldn't come down.",
 });
 messageList.push({
   title: "My house is on fire.",
-  address: "749 Baldwin Ave, San Francisco",
+  address: "Los Angeles, CA",
   rating: 1,
   imgURL: "https://media.giphy.com/media/3oeSAQoA5PLG4jLTos/giphy.gif",
   message: "and I can't contact my landlord.",
 });
 messageList.push({
   title: "My landlord's dog slapped me in the face.",
-  address: "324 Second Ave. New York, NY 10001",
+  address: "New York, NY 10001",
   rating: 2,
   imgURL: "https://media.giphy.com/media/AtwrcuevjPsys/giphy.gif",
-  message: "It still hurts.",
+  message: "and it hurt.",
 });
 messageList.push({
   title: "It's a cute cat but I'm allergic to cats",
-  address: "1234 Street Ave.",
+  address: "San Jose, CA",
   rating: 2,
   imgURL: "http://www.pixatu.com/wp-content/uploads/2017/03/wallpaper-cat-images-a%C2%B7-stock-photos-with-animal-emage-high-resolution-for-laptop.jpg",
   message: "The landlord's cat kept staring at me.",
 });
 messageList.push({
   title: "My landlord's pet monkey took down my drone.",
-  address: "52-43 Avenue St. Mountain View, CA",
+  address: "Mountain View, CA",
   rating: 2,
   imgURL: "https://media.giphy.com/media/moF5QkzLJGD04/giphy.gif",
   message: "The landlord's monkey took my drone down.",
 });
 messageList.push({
   title: "Look how dirty the bathroom is!",
-  address: "838 1st St. 5th Floor, San Francisco, CA 94110",
+  address: "Sausalito, CA 94110",
   rating: 1,
   imgURL: "https://media-cdn.tripadvisor.com/media/photo-s/07/1e/49/10/disgusting-bathrooms.jpg",
   message: "She's never around."
